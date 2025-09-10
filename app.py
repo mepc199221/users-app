db5bada9af56a08bc74e2cef131ab001ceb1c070
Roger <138404274+Kouriin1@users.noreply.github.com>
2025-09-09 22:27:22 -0400

    feat(apdate and add) user_email_search and app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from routes.user_email_search import product_search
+
 
 app = Flask(__name__)
+
+
+app.register_blueprint(product_search, url_prefix ='/user')
+
 
 @app.route('/')
 def index():
