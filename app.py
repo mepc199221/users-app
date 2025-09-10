85dd593a690d0fb76b3813aa80c41e34d6c30410
josefrometa <josel.frometa@gmail.com>
2025-09-09 22:31:38 -0400

    feat(add) user_list.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+from routes.user_list import user_list
 app = Flask(__name__)
+
+app.register_blueprint(user_list, url_prefix='/users')
 
 @app.route('/')
 def index():
