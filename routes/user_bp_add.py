b67eea204ad04d1191efdc3e5706597014cfacc8
Miguel Pérez <mperez@retailcompass.com>
2025-09-09 22:20:12 -0400

    feat (update) user_bp_add.py

--- routes/user_bp_add.py
+++ routes/user_bp_add.py
@@ -28,17 +28,7 @@ def fn_add_new_user():
 
 def _model_user_swap(req):
     return  {
-        "id":_generate_new_id_user(),
         "nombre": req['nombre'],
-        "precio": req['precio'],
-        "pantalla": req['pantalla'],
-        "bateria": req['bateria'],
-        "almacenamiento": req['almacenamiento']
+        "edad": req['edad'],
+        "email": req['email']
     }
-
-
-def _generate_new_id_user():
-    global next_id
-    cache = next_id
-    next_id += 1
-    return cache
