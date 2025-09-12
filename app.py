e713219a0b6cc70c86bafbe1574a09a1da212b74
elias <wayelias13@gmail.com>
2025-09-11 21:14:56 -0400

    feat: add default page 404

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from routes.user_list import user_list
 from routes.user_email_search import product_search
@@ -13,6 +13,15 @@ app.register_blueprint(user_list, url_prefix='/users')
 @app.route('/')
 def index():
     return jsonify ({"data":"user-app"})
+
+@app.errorhandler(404)
+def handle_404(e):
+response = {
+"error": "Not Found",
+"message": "La ruta solicitada no existe fuera de aqui mi loco",
+"status": 404
+}
+return jsonify(response), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
